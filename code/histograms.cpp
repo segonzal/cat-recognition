ef3bd3b1bffc5eb245969d5cc4d131dbc6c03b64
Jorge Bahamonde <jbahamon@ug.uchile.cl>
2014-11-06 16:16:45 -0300

    yay ahora uso archivos con listas de archivos xd

--- code/histograms.cpp
+++ code/histograms.cpp
@@ -15,6 +15,9 @@ String cat_folder = "./cats/train/";
 
 String out_name = "train";
 
+String cats_file = "./cats.txt";
+String dogs_file = "./dogs.txt";
+
 int dog_size= 1;
 int cat_size = 1;
 
@@ -55,13 +58,8 @@ int main (int argc, const char** argv)
 
     int K_values[4] = { 100, 500, 1000, 1500 };
 
-    stringstream filename;
-
     Ptr<FeatureDetector > detector(new SiftFeatureDetector());
-    Ptr<DescriptorMatcher > matcher(new BruteForceMatcher<L2<float> >());
-    Ptr<DescriptorExtractor> extractor(new
-            SiftDescriptorExtractor());
-    Ptr<BOWImgDescriptorExtractor> bowide(new BOWImgDescriptorExtractor(extractor,matcher));
+    Ptr<BOWImgDescriptorExtractor> bowides[4];
 
     FileStorage f("kmeans.yml", FileStorage::READ);
 
@@ -70,44 +68,91 @@ int main (int argc, const char** argv)
 
     vector<int> labels;
 
-    Mat histogram;
     Mat img, grayimg;
 
     for (int k_idx = 0; k_idx < 4; k_idx++) 
     {
         Mat vocabulary;
+        Ptr<DescriptorExtractor> extractor(new
+                SiftDescriptorExtractor());
+        Ptr<DescriptorMatcher > matcher(new BruteForceMatcher<L2<float> >());
+        Ptr<BOWImgDescriptorExtractor> bowide(new BOWImgDescriptorExtractor(extractor,matcher));
 
         param_name << "kmeans" << K_values[k_idx];
         f[param_name.str()] >> vocabulary;
         param_name.str(std::string());
-
         bowide->setVocabulary(vocabulary);
 
-        vector< vector<Mat> > histograms;
+        bowides[k_idx] = bowide;
 
-        for (int i = 0; i < dog_size; i++) 
+    }
+
+    ifstream dogfile(dogs_file.c_str());
+    vector< vector<Mat> > histograms[4];
+    string line;
+    Mat histogram;
+
+    while (getline(dogfile, line))
+    {
+
+        vector<Mat> image_histograms;
+        img = imread(line, CV_LOAD_IMAGE_COLOR);
+        cvtColor(img,grayimg,CV_BGR2GRAY);
+        Mat mask, bgdModel, fgdModel;
+
+        grabCut(img, mask, Rect(img.cols/10, img.rows/10, 
+                    (8 * img.cols)/10, (8 * img.rows)/10), 
+                    bgdModel, fgdModel, 1, GC_INIT_WITH_RECT);
+
+        detector->detect(grayimg,keypoints, mask);
+
+        for (int k_idx = 0; k_idx < 4; k_idx++)
         {
-            vector<Mat> image_histograms;
-            Mat histogram;
 
-            filename << dog_folder << i << ".jpg";
+            bowides[k_idx]->compute(grayimg, keypoints, histogram);
+            image_histograms.push_back(histogram);
 
-            img = imread(filename.str(), CV_LOAD_IMAGE_COLOR);
-            
-            cvtColor(img,grayimg,CV_BGR2GRAY);
-            
-            filename.str(std::string());
-            
-            Mat mask, bgdModel, fgdModel;
+            int hstep = grayimg.cols/2;
+            int vstep = grayimg.rows/2;
 
-            grabCut(img, mask, Rect(img.cols/10, img.rows/10, 
-                        (8 * img.cols)/10, (8 * img.rows)/10), 
-                        bgdModel, fgdModel, 1, GC_INIT_WITH_RECT);
-            
+            for (int m = 0; m < 2; m++) {
+                for (int n = 0; n < 2; n++) {
+                    Mat subimg(grayimg, cv::Rect(hstep * m, vstep * n, hstep, vstep));
+                    Mat submask(mask, cv::Rect(hstep * m, vstep * n, hstep, vstep));
 
-            detector->detect(grayimg,keypoints, mask);
-            bowide->compute(grayimg, keypoints, histogram);
+                    detector->detect(subimg, keypoints, submask);
+                    bowides[k_idx]->compute(subimg, keypoints, histogram);
+                    image_histograms.push_back(histogram);
+                
+                }
+            }
 
+            labels.push_back(0);
+            histograms[k_idx].push_back(image_histograms);
+        }
+    
+    }
+    
+    ifstream catfile(cats_file.c_str());
+
+    while (getline(catfile, line))
+    {
+
+        vector<Mat> image_histograms;
+        img = imread(line, CV_LOAD_IMAGE_COLOR);
+        cvtColor(img,grayimg,CV_BGR2GRAY);
+        Mat mask, bgdModel, fgdModel;
+
+        grabCut(img, mask, Rect(img.cols/10, img.rows/10, 
+                    (8 * img.cols)/10, (8 * img.rows)/10), 
+                    bgdModel, fgdModel, 1, GC_INIT_WITH_RECT);
+
+        detector->detect(grayimg,keypoints, mask);
+
+        for (int k_idx = 0; k_idx < 4; k_idx++)
+        {
+
+            bowides[k_idx]->compute(grayimg, keypoints, histogram);
             image_histograms.push_back(histogram);
 
             int hstep = grayimg.cols/2;
@@ -119,74 +164,30 @@ int main (int argc, const char** argv)
                     Mat submask(mask, cv::Rect(hstep * m, vstep * n, hstep, vstep));
 
                     detector->detect(subimg,keypoints, submask);
-                    bowide->compute(subimg, keypoints, histogram);
+                    bowides[k_idx]->compute(subimg, keypoints, histogram);
                     image_histograms.push_back(histogram);
                 
                 }
             }
 
             labels.push_back(1);
-            histograms.push_back(image_histograms);
-
+            histograms[k_idx].push_back(image_histograms);
         }
-        
-        for (int i = 0; i < cat_size; i++) 
-        {
-            vector<Mat> image_histograms;
-            Mat histogram;
+    
+    }
+    
 
-            filename << dog_folder << i << ".jpg";
-
-            img = imread(filename.str(), CV_LOAD_IMAGE_COLOR);
-            cvtColor(img,grayimg,CV_BGR2GRAY);
-
-            filename.str(std::string());
-             
-            Mat mask, bgdModel, fgdModel;
-
-            grabCut(img, mask, Rect(img.cols/10, img.rows/10, 
-                        (8 * img.cols)/10, (8 * img.rows)/10), 
-                        bgdModel, fgdModel, 1, GC_INIT_WITH_RECT);
-            
-
-            detector->detect(grayimg,keypoints, mask);
-            bowide->compute(grayimg, keypoints, histogram);
-
-            image_histograms.push_back(histogram);
-
-            int hstep = img.cols/2;
-            int vstep = img.rows/2;
-
-            for (int m = 0; m < 2; m++) {
-                for (int n = 0; n < 2; n++) {
-                    Mat subimg(grayimg, cv::Rect(hstep * m, vstep * n, hstep, vstep));
-                    Mat submask(mask, cv::Rect(hstep * m, vstep * n, hstep, vstep));
-
-                    detector->detect(subimg,keypoints, submask);
-                    bowide->compute(subimg, keypoints, histogram);
-                    image_histograms.push_back(histogram);
-                
-                }
-            }
-
-            labels.push_back(0);
-            histograms.push_back(image_histograms);
-
-        
-        }
-        
+    for (int k_idx = 0; k_idx < 4; k_idx++)
+    {
         param_name << out_name << "_" << K_values[k_idx];
 
         ofstream histograms_file;
         histograms_file.open(param_name.str().c_str());
         //save_sparse_descriptors(cout, labels, histograms);
-        save_sparse_descriptors(histograms_file, labels, histograms);
+        save_sparse_descriptors(histograms_file, labels, histograms[k_idx]);
         histograms_file.close();
         param_name.str(std::string());
-
+        f.release();
     }
-
-
-    f.release();
 
 }
